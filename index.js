e68a61ce649e1a6142ea188106a366087c06f234
dangillman <dangillman93@gmail.com>
2024-02-06 12:25:35 -0500

    added content to index.js, passed tests.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(activity) {
+    const actualActivity = activity ? `I will ${activity}` : "I will go to the office";
+    return `This Monday, ${actualActivity}.`;
+  }
+  function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
